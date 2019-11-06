f775d0458ed6b41e14d2a975a1494fecc3c2a5e5
tgiqfe <tqji31001@gmail.com>
2019-11-06 09:31:23 +0900

    Update Program class

--- Manifest/Program.cs
+++ Manifest/Program.cs
@@ -10,72 +10,90 @@ using System.IO.Compression;
 
 namespace Manifest
 {
-    //  V0.01.004
+    //  V0.01.005
     class Program
     {
-        const string PROJECT_NAME = "PSFile";
-
         static void Main(string[] args)
         {
             Environment.CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            string debugDir = string.Format(@"..\..\..\{0}\bin\Debug", PROJECT_NAME);
-            string releaseDir = string.Format(@"..\..\..\{0}\bin\Release", PROJECT_NAME);
-            string moduleDir = string.Format(@"..\..\..\{0}\bin\{0}", PROJECT_NAME);
-            string moduleZip = string.Format(@"..\..\..\{0}\bin\{0}.zip", PROJECT_NAME);
+            ProjectInfo info = new ProjectInfo();
 
-            PSD1.Create(PROJECT_NAME, debugDir);
-            PSD1.Create(PROJECT_NAME, releaseDir);
-            PSM1.Create(PROJECT_NAME, debugDir);
-            PSM1.Create(PROJECT_NAME, releaseDir);
+            PSD1.Create(info.ProjectName, info.DebugDir);
+            PSD1.Create(info.ProjectName, info.ReleaseDir);
+            PSM1.Create(info.ProjectName, info.DebugDir);
+            PSM1.Create(info.ProjectName, info.ReleaseDir);
 
             //  Releaseフォルダーを公開用にコピー
-            if (Directory.Exists(releaseDir))
+            if (Directory.Exists(info.ReleaseDir))
             {
                 using (Process proc = new Process())
                 {
                     proc.StartInfo.FileName = "robocopy.exe";
                     proc.StartInfo.Arguments = string.Format(
-                        "\"{0}\" \"{1}\" /COPY:DAT /MIR /E /XJD /XJF", releaseDir, moduleDir);
+                        "\"{0}\" \"{1}\" /COPY:DAT /MIR /E /XJD /XJF", info.ReleaseDir, info.ModuleDir);
                     proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
                     proc.Start();
                     proc.WaitForExit();
                 }
             }
 
-            //  Scriptフォルダーをサンプルスクリプトフォルダーにコピー
-            string scriptDir = string.Format(@"..\..\..\{0}\Script", PROJECT_NAME);
-            if (Directory.Exists(scriptDir))
+            //  Scriptフォルダーをコピー
+            if (Directory.Exists(info.ScriptDir))
             {
+                foreach (string fileName in Directory.GetFiles(info.ScriptDir))
+                {
+                    File.Copy(fileName, Path.Combine(info.ModuleDir, Path.GetFileName(fileName)), true);
+                }
                 using (Process proc = new Process())
                 {
                     proc.StartInfo.FileName = "robocopy.exe";
-                    proc.StartInfo.Arguments = string.Format(
-                        "\"{0}\" \"{1}\\SampleScript\" /COPY:DAT /MIR /E /XJD /XJF", scriptDir, moduleDir);
                     proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    proc.Start();
-                    proc.WaitForExit();
+
+                    foreach (string dirName in Directory.GetDirectories(info.ScriptDir))
+                    {
+                        proc.StartInfo.Arguments = string.Format(
+                            "\"{0}\" \"{1}\" /COPY:DAT /MIR /E /XJD /XJF", 
+                            dirName, 
+                            Path.Combine(info.ModuleDir, Path.GetFileName(dirName)));
+                        proc.Start();
+                        proc.WaitForExit();
+                    }
                 }
             }
 
             //  フォーマットファイルをコピー
-            string formatDir = string.Format(@"..\..\..\{0}\Format", PROJECT_NAME);
-            if (Directory.Exists(formatDir))
+            if (Directory.Exists(info.FormatDir))
             {
-                foreach (string fileName in Directory.GetFiles(formatDir, "*.ps1xml"))
+                foreach (string fileName in Directory.GetFiles(info.FormatDir, "*.ps1xml"))
                 {
-                    File.Copy(fileName, Path.Combine(moduleDir, Path.GetFileName(fileName)), true);
+                    File.Copy(fileName, Path.Combine(info.FormatDir, Path.GetFileName(fileName)), true);
+                }
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo.FileName = "robocopy.exe";
+                    proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                    foreach (string dirName in Directory.GetDirectories(info.FormatDir))
+                    {
+                        proc.StartInfo.Arguments = string.Format(
+                            "\"{0}\" \"{1}\" /COPY:DAT /MIR /E /XJD /XJF",
+                            dirName,
+                            Path.Combine(info.ModuleDir, Path.GetFileName(dirName)));
+                        proc.Start();
+                        proc.WaitForExit();
+                    }
                 }
             }
 
             //  モジュールフォルダーをZipアーカイブ
-            if (Directory.Exists(moduleDir))
+            if (Directory.Exists(info.ModuleDir))
             {
-                if (File.Exists(moduleZip))
+                if (File.Exists(info.ModuleZip))
                 {
-                    File.Delete(moduleZip);
+                    File.Delete(info.ModuleZip);
                 }
-                ZipFile.CreateFromDirectory(moduleDir, moduleZip, CompressionLevel.NoCompression, false);
+                ZipFile.CreateFromDirectory(info.ModuleDir, info.ModuleZip, CompressionLevel.NoCompression, false);
             }
         }
     }
---
+++ Manifest/ProjectInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manifest
+{
+    public class ProjectInfo
+    {
+        public string ProjectName { get; set; } //  = "プロジェクト名を指定";
+        public string DebugDir { get { return string.Format(@"..\..\..\{0}\bin\Debug", ProjectName); } }
+        public string ReleaseDir { get { return string.Format(@"..\..\..\{0}\bin\Release", ProjectName); } }
+        public string ModuleDir { get { return string.Format(@"..\..\..\{0}\bin\{0}", ProjectName); } }
+        public string ModuleZip { get { return string.Format(@"..\..\..\{0}\bin\{0}.zip", ProjectName); } }
+        public string ScriptDir { get { return string.Format(@"..\..\..\{0}\Script", ProjectName); } }
+        public string FormatDir { get { return string.Format(@"..\..\..\{0}\Format", ProjectName); } }
+
+        public ProjectInfo() { }
+    }
+}
